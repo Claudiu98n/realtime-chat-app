0822b727d612668f6b9427e5b108c254ceaa0123
Claudiu98n <claudiuneagu9988@gmail.com>
2022-03-21 12:30:36 +0200

    database port

--- packages/server/database/index.js
+++ packages/server/database/index.js
@@ -3,6 +3,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: "db",
   dialect: "postgres",
+  port: "5432",
 });
 
 (async () => {
